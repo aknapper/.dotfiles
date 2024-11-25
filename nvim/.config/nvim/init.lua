4aa2bb0016a73e7b7e1faadc7f2e782d6d1329df
Aaron Knapper <34011307+aknapper@users.noreply.github.com>
2024-11-24 21:04:57 -0500

    add telescope to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,3 +12,10 @@ vim.cmd.colorscheme "nordic"
 
 -- true colour
 vim.o.termguicolors = true
+
+-- telescope usage and keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
